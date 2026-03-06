5025a178433007a0c4f8fdeae7799eb91f017dda
Mohit <mohitswarnkar13@gmail.com>
2026-03-06 17:29:07 +0530

    add debounce mechanism to prevent excessive rebuilds

---
+++ internal/debounce/debounce.go
@@ -0,0 +1,30 @@
+package debounce
+
+import (
+	"sync"
+	"time"
+)
+
+type Debouncer struct {
+	delay time.Duration
+	timer *time.Timer
+	mu    sync.Mutex
+}
+
+func New(ms int) *Debouncer {
+	return &Debouncer{
+		delay: time.Duration(ms) * time.Millisecond,
+	}
+}
+
+func (d *Debouncer) Trigger(fn func()) {
+
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.timer != nil {
+		d.timer.Stop()
+	}
+
+	d.timer = time.AfterFunc(d.delay, fn)
+}
